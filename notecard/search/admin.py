91eb754c74a0b94f0c7805ac3edac0441f9179a6
DanHoerst <DHoerst1@gmail.com>
2012-09-14 10:11:41 -0400

    search model removal

--- notecard/search/admin.py
+++ notecard/search/admin.py
@@ -1,9 +1,9 @@
-from notecard.search.models import NotecardKeyword
-from django.contrib import admin
-from django.contrib.flatpages.models import FlatPage
+#from notecard.search.models import NotecardKeyword
+#from django.contrib import admin
+#from django.contrib.flatpages.models import FlatPage
 
 # Sets up 3 inline keywords using SearchKeyWord model
-class NotecardKeywordInline(admin.StackedInline):
-    model = NotecardKeyword
-    extra = 3
+#class NotecardKeywordInline(admin.StackedInline):
+#    model = NotecardKeyword
+#    extra = 3
 
